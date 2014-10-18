286a5bfca5039ff6b02057236e2eba5649537c52
miyanagi urushi <tenkoumituru@gmail.com>
2014-10-18 17:52:47 +0900

    a

--- app/src/main/java/com/example/myapplication/MyActivity.java
+++ app/src/main/java/com/example/myapplication/MyActivity.java
@@ -2,18 +2,81 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 
 public class MyActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        /*button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    Thread.sleep(10000);
+                }catch (InterruptedException e){
+                    Log.v("HeavyTask", "error");
+                }
+
+                TextView textview1 = (TextView)findViewById(R.id.textView1);
+                textview1.setText("Hello Thread");
+                textview1.setVisibility(View.VISIBLE);
+            }
+        });*/
+/*        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        heavyTask();
+                    }
+                }).start();
+
+                TextView textview1 = (TextView)findViewById(R.id.textView1);
+                textview1.setText("Hello Thread");
+                textview1.setVisibility(View.VISIBLE);
+            }
+        });*/
+        Handler handler = new Handler() {
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public void flush() {
+
+            }
+
+            @Override
+            public void publish(LogRecord logRecord) {
+
+            }
+        };
     }
 
+    private void heavyTask(){
+        try{
+            Thread.sleep(10000);
+        }catch (InterruptedException e){
+            Log.v("HeavyTask","error");
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
